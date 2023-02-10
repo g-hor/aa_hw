b77e171080872abc18925f0d6e9dfac55b697621
Gary <geehor@gmail.com>
2023-02-10 09:42:20 -0500

    Finish Movie Buff

--- W5D5/practice-for-ch-sql-movie-buff-in-training/movie_novice/queries.rb
+++ W5D5/practice-for-ch-sql-movie-buff-in-training/movie_novice/queries.rb
@@ -36,20 +36,20 @@ end
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-  Movie.find_by_sql(['SELECT title FROM movies WHERE score >= 9.0'])
+  Movie.where('score >= ?', 9).select('title')
 end
 
 def star_wars
   # display the id, title, and year of each Star Wars movie in movies
   # hint: use 'select' and 'where'
   
-  # Movie.where("title LIKE ?", "Star Wars" + "%")
-  Movie.find_by_sql([
-    'SELECT movies.id, movies.title, movies.yr
-    FROM movies
-    WHERE title LIKE ?',
-    "Star Wars" + "%"
-  ])
+  # Movie.find_by_sql([
+  #   'SELECT movies.id, movies.title, movies.yr
+  #   FROM movies
+  #   WHERE title LIKE ?',
+  #   "Star Wars" + "%"
+  # ])
+  Movie.where('title LIKE ?', 'Star Wars' + '%').select('id, title, yr')
 end
 
 
