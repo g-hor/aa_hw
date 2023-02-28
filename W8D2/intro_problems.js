21dcab08db46f619942e5446cf68c2ae470b684d
Gary <geehor@gmail.com>
2023-02-28 05:15:16 -0500

    W8D2 JS Intro

---
+++ W8D2/intro_problems.js
@@ -0,0 +1,62 @@
+function madLib(verb, adj, noun) {
+  console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}`);
+}
+
+function isSubstring(searchString, subString) {
+  if (searchString.includes(subString)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function fizzBuzz(arr) {
+  let newarr = []
+
+  arr.forEach(ele => {
+    if (ele % 3 == 0 || ele % 5 == 0) {
+      newarr.push(ele)
+    }
+  });
+
+  return newarr
+}
+
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false
+      break
+    }
+  }
+  return true
+}
+
+
+// function sumOfNPrimes(n) {
+//   let sum = 0
+//   let add_count = 0
+//   while (add_count < n) {
+//     var prime = 2
+//     if (isPrime(prime)) {
+//       sum += prime
+//       add_count++
+//     }
+//     prime++
+//   }
+
+//   return sum
+// }
+
+function sumOfNPrimes(n) {
+  const primes = []
+  for (let i = 0; i < n; i++) {
+    
+  }
+}
+
+
+// madLib('make', 'best', 'guac');
+// isSubstring("time to program", "time");
+// isSubstring("Jump for joy", "joys");
